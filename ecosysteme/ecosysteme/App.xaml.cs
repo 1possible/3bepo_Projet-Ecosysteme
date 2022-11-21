c0b17754d5b8395eae4f7bdb94c2cba3b83cd95f
Emilie Van Bogaert <21258@ecam.be>
2022-11-21 09:57:11 +0100

    basic maui project

---
+++ ecosysteme/ecosysteme/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ecosysteme;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ecosysteme/ecosysteme/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ecosysteme;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
