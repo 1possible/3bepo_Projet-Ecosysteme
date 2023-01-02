4c191cf3087c679314bb21177bcaa9ed08da9316
Emilie Van Bogaert <21258@ecam.be>
2023-01-02 11:21:45 +0100

    Create IComportement
    create the interface for the comportement

---
+++ ecosysteme/ecosysteme/Models/IComportement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecosysteme.Models
+{
+    internal interface IComportement<T>
+    {
+        enum etatComportementBase
+        {
+            None = 0,
+            Alimentation = 1,
+            reproduction = 2
+        }
+
+
+        abstract public void UpdateEtat(T thisObject);
+
+        abstract protected bool AvailableAlimentation(T thisObject);
+        abstract protected void ActionAlimentation(T thisObject);
+        abstract protected bool AvailableReproduction(T thisObject);
+        abstract protected void ActionReproduction(T thisObject);
+    }
+}
